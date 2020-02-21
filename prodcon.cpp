71029ecc5f30ff344d9a1c5d90be5fc1e8f8f1b7
Luke <kapeluck@ualberta.ca>
2020-02-20 19:18:28 -0700

    Testing threads, mutexes, atomics and imports

--- prodcon.cpp
+++ prodcon.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>	// atoi
 #include <cctype>	// isdigit
 #include <string>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
 
 /* Declare the external C file's functions */
 extern "C" {
@@ -10,6 +15,18 @@ extern "C" {
 };
 
 using namespace std;
+
+atomic<bool> * asking_for_work;	// Atomic objects used by the producer to see if everything is done
+								// Used to make sure a safe exit occurs
+mutex printing_mutex;			// Used to control who is printing and who is waiting to print
+mutex work_queue_mutex;			// Used to control who is accessing the queue for work
+queue<int> *work_queue;			// The queue for the work
+
+void thread_method(int id) {
+	printing_mutex.lock();
+	printf("Thread %d says hello.\n", id);
+	printing_mutex.unlock();
+}
 
 int main(int argc, char *argv[]) {
 
@@ -20,20 +37,37 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	int nthreads = atoi(argv[1]);
+	const int nthreads = atoi(argv[1]);
 	string id = "0";
 
 	if (argc >= 3) {
 		if ( isdigit( argv[2][0] ) ) {
 			id = argv[2];
-//			id = "666";
 		}
 	}
 
-	printf("You entered details NTHREADS = %d, ID = %s\n", nthreads, id.c_str());
+	// printf("You entered details NTHREADS = %d, ID = %s\n", nthreads, id.c_str());
 
-	/* START THE PROCESS */
-	/* READ FROM STDIN AND PERFORM THE TASKS */
+	/* START OF PRODCON */
+	/* SPAWN THE CONSUMER THREADS AND SAVE THEIR STATES */
+
+	asking_for_work = new atomic<bool>[nthreads];
+
+	for (int i = 0; i < nthreads; i++) {
+		asking_for_work[i].store(false, memory_order_release);
+	}
+
+	thread consumers[nthreads];
+
+	for (int i = 0; i < nthreads; i++) {
+		consumers[i] = thread(thread_method, i);
+	}
+
+	for (int i = 0; i < nthreads; i++) {
+		consumers[i].join();
+	}
+
+	printf("The consumers have finished all their work.\n");
 
 	return 0;
 }
