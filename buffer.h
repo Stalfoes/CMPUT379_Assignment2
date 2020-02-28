d06f7ad49850d437579fae82557992da259f8939
Luke <kapeluck@ualberta.ca>
2020-02-28 09:35:05 -0700

    Moved buffer code to buffer.h

---
+++ buffer.h
@@ -0,0 +1,51 @@
+#ifndef __BUFFER_H__
+#define __BUFFER_H__
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+#define NO_MORE_WORK -1
+
+class Buffer {
+public:
+	Buffer(int max_size) : max_size(max_size) { }
+	~Buffer() { }
+
+	void push(int work) {
+		unique_lock<mutex> lk(lock);
+		cv.wait(lk, [this]() { return !isFull(); } );
+		work_queue.push(work);
+		lk.unlock();
+		cv.notify_all();
+	}
+
+	void pop(int& work) {
+		unique_lock<mutex> lk(lock);
+		cv.wait(lk, [this]() { return !isEmpty(); } );
+		work = work_queue.front();
+		if (work != NO_MORE_WORK)
+			work_queue.pop();
+		lk.unlock();
+		cv.notify_all();	
+	}
+
+private:
+
+	bool isFull() const {
+		return work_queue.size() >= max_size;
+	}
+
+	bool isEmpty() const {
+		return work_queue.size() == 0;
+	}
+
+	int max_size;
+	mutex lock;
+	queue<int> work_queue;
+	condition_variable cv;
+};
+
+#endif
