2ff266c8ccc5d277442a5a7b61ca297b5da78688
Luke <kapeluck@ualberta.ca>
2020-02-29 17:16:02 -0700

    Added timing test - untested

--- timetest.cpp
+++ timetest.cpp
@@ -1,4 +1,29 @@
 #include <cstdio>
 #include <ctime>
+#include <chrono>
+#include <thread>
 
+using namespace std;
 
+int main() {
+
+	clock_t startClock = clock();
+	
+	chrono::milliseconds startTime = chrono::duration_cast<std::milliseconds> ( 
+		chrono::system_clock::now().time_since_epoch()
+	);
+
+	this_thread::sleep_for(chrono::seconds(3));
+
+	float dc = ((float)(clock() - program_start)) / CLOCKS_PER_SEC;
+
+	chrono::milliseconds dt_ms = chrono::duration_cast<std::milliseconds> ( 
+		chrono::system_clock::now().time_since_epoch()
+	) - startTime;
+
+	float dt = ((float) dt_ms.count()) / 1000;
+
+	printf("CPU:  %.5f\nREAL: %.5f\n", dc, dt);
+
+	return 0;
+}
